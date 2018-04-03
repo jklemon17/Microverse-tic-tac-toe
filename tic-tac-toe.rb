3ce219d2fb5f2969dbdb97d6375332a23d7f70b9
PolarisTLX <paul.a.rail@gmail.com>
2018-04-03 09:30:03 -0400

    All win conditions working and prompt to play again

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,12 +1,16 @@
 #This is our game file
 class TicTacToe
   attr_accessor :turn, :player1, :player2, :squares
-  def initialize
+  def initialize (player1, player2)
     @turn = "X"
-    puts "Please enter Player 1's name:"
-    @player1 = Player.new(gets.chomp, "X")
-    puts "Please enter Player 2's name:"
-    @player2 = Player.new(gets.chomp, "O")
+    # puts "Please enter Player 1's name:"
+    # @player1 = Player.new(gets.chomp, "X")
+    # puts "Please enter Player 2's name:"
+    # @player2 = Player.new(gets.chomp, "O")
+
+    @player1 = player1
+    @player2 = player2
+
     # build the grid
     @squares = {}
     ('a'..'c').each do |letter|
@@ -29,6 +33,10 @@ class TicTacToe
   end
 
   def update_square(marker, target)
+
+    # if player wants to quit
+    # use .downcase to account for "Q" and "q".  [0] is to check only the first letter, to account for if user types the whole word "Quit"
+    return if target.downcase[0] == "q"
 
     # check if valid input
     if @squares.keys.include?(target) == false
@@ -64,17 +72,35 @@ class TicTacToe
 
   def game_over
     # check the rows
-    if @squares["a1"].state == "X" && @squares["a2"].state == @squares["a1"].state && @squares["a3"].state == @squares["a1"].state
-        true
+    if @squares["a1"].state != "_" && @squares["a2"].state == @squares["a1"].state && @squares["a3"].state == @squares["a1"].state
+        return true
     end
     if @squares["b1"].state != "_" && @squares["b2"].state == @squares["b1"].state && @squares["b3"].state == @squares["b1"].state
-        true
+        return true
     end
     if @squares["c1"].state != "_" && @squares["c2"].state == @squares["c1"].state && @squares["c3"].state == @squares["c1"].state
-        true
+        return true
     end
+
     # check the columns
+    if @squares["a1"].state != "_" && @squares["b1"].state == @squares["a1"].state && @squares["c1"].state == @squares["a1"].state
+      return true
+    end
+    if @squares["a2"].state != "_" && @squares["b2"].state == @squares["a2"].state && @squares["c2"].state == @squares["a2"].state
+      return true
+    end
+    if @squares["a3"].state != "_" && @squares["b3"].state == @squares["a3"].state && @squares["c3"].state == @squares["a3"].state
+      return true
+    end
+
     # check the diagonals
+    if @squares["a1"].state != "_" && @squares["b2"].state == @squares["a1"].state && @squares["c3"].state == @squares["a1"].state
+      return true
+    end
+    if @squares["a3"].state != "_" && @squares["b2"].state == @squares["a3"].state && @squares["c1"].state == @squares["a3"].state
+      return true
+    end
+
   end
 end
 
@@ -104,23 +130,38 @@ end
 
 
 
+puts "Please enter Player 1's name:"
+player1 = Player.new(gets.chomp, "X")
+puts "Please enter Player 2's name:"
+player2 = Player.new(gets.chomp, "O")
 
-current_game = TicTacToe.new
+# this will perpetually reset the game until a win condition occurs and the user types "N" when asked if they want to continue.
+loop do
 
-puts "Player 1's name is: #{current_game.player1.name}. They are using the #{current_game.player1.marker}"
-puts "Player 2's name is: #{current_game.player2.name}. They are using the #{current_game.player2.marker}"
+    # puts "Player 1's name is: #{current_game.player1.name}. They are using the #{current_game.player1.marker}"
+    # puts "Player 2's name is: #{current_game.player2.name}. They are using the #{current_game.player2.marker}"
+    puts "\n"
+    puts "Player 1's name is: #{player1.name}. They are using the #{player1.marker}"
+    puts "Player 2's name is: #{player2.name}. They are using the #{player2.marker}"
 
-current_game.display_grid
+    current_game = TicTacToe.new(player1, player2)
 
-until current_game.game_over
-  target_square = current_game.ask_player()
-  current_game.update_square(current_game.turn, target_square)
-  current_game.display_grid
-  current_game.change_turn
+    current_game.display_grid
+
+    until current_game.game_over
+      target_square = current_game.ask_player()
+      current_game.update_square(current_game.turn, target_square)
+      current_game.display_grid
+      current_game.change_turn
+    end
+
+    puts "#{current_game.turn == "X" ? current_game.player2.name : current_game.player1.name} Wins!"
+    puts "Want to play again with same players?  Y/N"
+    # use .downcase to account for "N" and "n".  [0] is to check only the first letter, to account for if user types the whole word "no"
+    break if gets.chomp.downcase[0] == "n"
+
 end
-
-puts "#{current_game.turn == "X" ? current_game.player1.name : current_game.player2.name} Wins!"
-
+puts "\nThanks for playing!"
 
 
 # update_square(marker, target)
