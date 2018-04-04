8f62f53b3886a8c8be75cc0bc2043a8ef4506a04
PolarisTLX <paul.a.rail@gmail.com>
2018-04-04 10:46:58 -0400

    Accounted for user not entering a name + added test for the square class

--- lib/tic-tac-toe.rb
+++ lib/tic-tac-toe.rb
@@ -105,10 +105,19 @@ class Player
 end
 
 # Begin game
+# puts "Please enter Player 1's name:"
+# player1 = Player.new(gets.chomp, "X")
+# puts "Please enter Player 2's name:"
+# player2 = Player.new(gets.chomp, "O")
+
+# redone to account for if the user does not enter a name at all and presses enter anyways:
+# solved by prividing a default value:
 puts "Please enter Player 1's name:"
-player1 = Player.new(gets.chomp, "X")
+name = gets.chomp
+player1 = Player.new(name == "" ? "Player 1" : name, "X")
 puts "Please enter Player 2's name:"
-player2 = Player.new(gets.chomp, "O")
+name = gets.chomp
+player2 = Player.new(name == "" ? "Player 2" : name, "O")
 
 # This will perpetually reset the game until a win condition occurs and the user types "N" when asked if they want to continue or the player types "quit"
 loop do
--- spec/tic-tac-toe_spec.rb
+++ spec/tic-tac-toe_spec.rb
@@ -1,6 +1,8 @@
 require 'tic-tac-toe'
 
 describe Player do
+
+  # build one instance of a player first
   before do
     @player = Player.new("Paul", "X")
   end
@@ -14,12 +16,41 @@ describe Player do
       end
     end
   end
+
   describe "#marker=" do
     context "trying to change marker" do
       it "raises a NoMethodError" do
         # Both tests below will pass, the second is just more specific
         #expect { @player.marker="O" }.to raise_error
         expect { @player.marker="O" }.to raise_error(NoMethodError)
+      end
+    end
+  end
+
+end
+
+describe Square do
+
+  # build one instance of a Square first
+  before do
+    @square = Square.new("a1")
+  end
+
+
+  describe ".new" do
+    context "given a instance of  square" do
+      it "should have a name and a initial state" do
+        # Testing that player name and marker initialize correctly.
+        expect([@square.name, @square.state]).to eql(["a1", "_"])
+      end
+    end
+  end
+
+  describe "#name=" do
+    context "trying to change name" do
+      it "raises a NoMethodError" do
+        # Testing that the name of a square cannot be changed
+        expect { @square.name="c3" }.to raise_error(NoMethodError)
       end
     end
   end
